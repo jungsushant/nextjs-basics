d2f73da675a7b8efc199ae8d42b8394ae943328c
Sushant Thapa <kabitashahi2022@whitehousecmt.edu.np>
2023-11-10 10:57:52 +0545

    Nested Routing basics

---
+++ app/login/student/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const studentLogin = () => {
+  return <div>studentLogin</div>;
+};
+
+export default studentLogin;
---
+++ app/login/teacher/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const teacherLogin = () => {
+  return <div>teacherLogin</div>;
+};
+
+export default teacherLogin;
