6afebe578c09101ddd28219a99fa2b49513a11f6
Sushant Thapa <kabitashahi2022@whitehousecmt.edu.np>
2023-11-10 10:09:56 +0545

    useRouter hook basics

--- app/login/page.js
+++ app/login/page.js
@@ -1,12 +1,18 @@
-'use client'
+"use client";
 
-import Link from "next/link"
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const page = () => {
+  const router = useRouter();
   return (
-    <div><h1>Login Page</h1>
-    <button><Link href='/'>Home page</Link></button></div>
-  )
-}
+    <div>
+      <h1>Login Page</h1>
+      <button>
+        <Link href="/">Home page</Link>
+      </button>
+    </div>
+  );
+};
 
-export default page
+export default page;
--- app/page.js
+++ app/page.js
@@ -1,24 +1,27 @@
-'use client'
-import Image from 'next/image'
-import Link from 'next/link'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const apple=()=> {
-    alert("hello")
-  }
+  const apple = () => {
+    alert("hello");
+  };
 
-  const InnerComp =()=> {
-    return (
-      <h1>hello</h1>
-    )
-  }
+  const InnerComp = () => {
+    return <h1>hello</h1>;
+  };
+  const router = useRouter();
   return (
     <>
-    <h1>Events, function and state</h1>
-    <button onClick={()=> (apple)}>click me</button>
-    {<InnerComp/>}
-    <button><Link href='/login'>Login page</Link></button>
-
+      <h1>Events, function and state</h1>
+      <button onClick={() => apple}>click me</button>
+      {<InnerComp />}
+      <button>
+        <Link href="/login">Login page</Link>
+      </button>
+      <br />
+      <button onClick={() => router.push("/about")}>go to about us page</button>
     </>
-  )
+  );
 }
