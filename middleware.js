30f57cab2a06092e8cafe016d0721e69aa0acba9
Sushant Thapa <kabitashahi2022@whitehousecmt.edu.np>
2023-11-10 22:29:36 +0545

    confing in middleware done

--- middleware.js
+++ middleware.js
@@ -1,7 +1,13 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  if (request.nextUrl.pathname != "/home") {
-    return NextResponse.redirect(new URL("/home", request.url));
-  }
+  //   if (request.nextUrl.pathname != "/home") {
+  return NextResponse.redirect(new URL("/", request.url));
+  //   }
 }
+
+// applying middleware to matching urls only
+
+export const config = {
+  matcher: ["/about/:path", "/login/:path"],
+};
