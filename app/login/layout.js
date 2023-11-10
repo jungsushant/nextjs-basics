829044f03797dc2cead96826569073742c8841bb
Sushant Thapa <kabitashahi2022@whitehousecmt.edu.np>
2023-11-10 17:15:11 +0545

    Conditionally deleted common layout for student login path

--- app/login/layout.js
+++ app/login/layout.js
@@ -1,7 +1,14 @@
+"use client";
+import { usePathname } from "next/navigation";
+
 export default function layout({ children }) {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <div>
-      <h1>Common layout for login pages</h1>
+      {pathname !== "/login/student" ? (
+        <h1>Common layout for login pages</h1>
+      ) : null}
       {children}
     </div>
   );
