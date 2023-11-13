a8ffa6bcd8768c04b7fede26c662851e3490bce8
Sushant Thapa <kabitashahi2022@whitehousecmt.edu.np>
2023-11-13 17:31:09 +0545

    data fetch on client side componient done

--- app/productList/page.js
+++ app/productList/page.js
@@ -1,14 +1,29 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const page = () => {
   const navigate = useRouter();
+  const [ddata, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      let data = await fetch("https://dummyjson.com/products");
+      data = await data.json();
+      setData(data.products);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div>
       page
       <br />
-      <button onClick={() => navigate.push((href = "/"))}>Go to Home</button>
+      <button onClick={() => navigate.push("/")}>Go to Home</button>
+      {ddata.map((item) => {
+        return <h1 key={item.id}>{item.title}</h1>;
+      })}
     </div>
   );
 };
